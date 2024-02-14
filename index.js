6efb7a2b394f59ead52ca231b82e4ee40d9c1804
Mirsaid <160034290+BaxodirovMirsaid@users.noreply.github.com>
2024-02-14 21:14:50 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let z = Math.floor(Math.random() *10)
+console.log(z)
+
+
+let numberIsUser = +prompt("Raqam kirirting..")
+
+if(numberIsUser == z) {
+    alert("Siz yutdingiz🎉🎉🎉🎉🎉🎉🎉🎉")
+}
+else{
+    alert("Siz yuta olmadingiz qayta urinib ko'ring😒😒😒😒")
+}
